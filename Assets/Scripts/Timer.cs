1fbc669c50ef465a9967c9a44309f9dd56b8dfe9
radical-ish <cruzmatt1015@gmail.com>
2026-04-06 10:32:36 -0500

    added timer and win/lose condition

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,61 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [Serializable]
+    protected struct TimeFormat
+    {
+        public int minutes;
+        [Range(0, 59)] public float seconds;
+    }
+
+    [SerializeField] private TimeFormat timeLimit;
+    [SerializeField] private TMP_Text timerText;
+    [SerializeField] private int winIndex;
+    [SerializeField] private int loseIndex;
+
+    private void Start()
+    {
+        timerText = GetComponentInChildren<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        timerCountdown();
+    }
+
+    private void timerCountdown()
+    {
+        if (timeLimit.minutes <= 0 && timeLimit.seconds <= 0)
+        {
+            timerText.text = "0:00";
+            winCheck();
+            return;
+        }
+
+        timeLimit.seconds -= Time.deltaTime;
+
+        if (timeLimit.seconds < 0 && timeLimit.minutes > 0)
+        {
+            timeLimit.minutes--;
+            timeLimit.seconds = 59;
+        }
+
+        timerText.text = timeLimit.minutes + ":" + (timeLimit.seconds < 10 ? "0" : "") + Mathf.Round(timeLimit.seconds);
+    }
+
+    private void winCheck()
+    {
+        if(PizzaDeliveryHandler.instance.Money >= PizzaDeliveryHandler.instance.Rent)
+        {
+            SceneManager.LoadScene(winIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene(loseIndex);
+        }
+    }
+}
