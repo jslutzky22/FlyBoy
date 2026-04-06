f9d4fe017d70cf5185d10f70cfe954c79a44578b
Juke422B <jherron@mail.bradley.edu>
2026-04-06 11:05:53 -0500

    skill checks work

--- Assets/Scripts/MasterSwatter.cs
+++ Assets/Scripts/MasterSwatter.cs
@@ -3,45 +3,500 @@ using UnityEngine;
 
 public class MasterSwatter : MonoBehaviour
 {
+    [Header("Main Variables")]
     [SerializeField] private float swatterEventSystemTimer;
     private int swatterIndex = 0;
+    public bool flyBoyMadeItToEvent;
+
+    [Header("City Health")]
+    private int cityHealth;
+    private int cityHealAmount;
+
+    [Header("Events")]
+    private bool eventActive;
+    [SerializeField] private GameObject[] eventZeroObjects;
+    [SerializeField] private GameObject[] eventOneObjects;
+    [SerializeField] private GameObject[] eventTwoObjects;
+    [SerializeField] private GameObject[] eventThreeObjects;
+    [SerializeField] private GameObject[] eventFourObjects;
+    [SerializeField] private GameObject[] eventFiveObjects;
+    [SerializeField] private GameObject[] eventSixObjects;
+    [SerializeField] private int eventWaitTimer;
 
     void Start()
     {
-        StartCoroutine(SwatterEventSystem());    
+        StartCoroutine(SwatterEventSystem());
+        cityHealth = 100;
     }
 
     IEnumerator SwatterEventSystem()
     {
         yield return new WaitForSecondsRealtime(swatterEventSystemTimer);
-        swatterIndex = Random.Range(0, 6);
-        if (swatterIndex == 0)
+        swatterIndex = Random.Range(0, 7);
+        if (swatterIndex == 0 && !eventActive)
+        {
+            eventActive = true;
+            StartCoroutine(EventZero());
+        }
+        else if (swatterIndex == 1 && !eventActive)
+        {
+            eventActive = true;
+            StartCoroutine(EventOne());
+        }
+        else if (swatterIndex == 2 && !eventActive)
+        {
+            eventActive = true;
+            StartCoroutine(EventTwo());
+        }
+        else if (swatterIndex == 3 && !eventActive)
+        {
+            eventActive = true;
+            StartCoroutine(EventThree());
+        }
+        else if (swatterIndex == 4 && !eventActive)
+        {
+            eventActive = true;
+            StartCoroutine(EventFour());
+        }
+        else if (swatterIndex == 5 && !eventActive)
+        {
+            eventActive = true;
+            StartCoroutine(EventFive());
+        }
+        else if (swatterIndex == 6 && !eventActive)
+        {
+            eventActive = true;
+            StartCoroutine(EventSix());
+        }
+        while (!eventActive)
         {
 
         }
-        else if (swatterIndex == 1)
-        {
+        StartCoroutine(SwatterEventSystem());
+    }
 
+    private void CityHeal()
+    {
+        cityHealth += cityHealAmount;
+    }
+
+    IEnumerator EventZero()
+    {
+        int arrayLength = 0;
+        while (arrayLength < eventZeroObjects.Length)
+        {
+            eventZeroObjects[arrayLength].SetActive(true);
+            arrayLength++;
         }
-        else if (swatterIndex == 2)
+        int timer = eventWaitTimer;
+        while (timer > 0 && !flyBoyMadeItToEvent)
         {
-
+            yield return new WaitForSecondsRealtime(1f);
+            timer--;
         }
-        else if (swatterIndex == 3)
+        if (flyBoyMadeItToEvent)
         {
-
+            FindAnyObjectByType<SkillCheck>().SkillCheckActivate();
+            while (FindAnyObjectByType<SkillCheck>().skillCheckActive)
+            {
+                yield return null;
+            }
+            if (FindAnyObjectByType<SkillCheck>().skillCheckHit)
+            {
+                FindAnyObjectByType<SkillCheck>().skillCheckHit = false;
+                CityHeal();
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
+            else 
+            {
+                //insert a value here, i just don't know what to put yet
+                cityHealth -= 0;
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
         }
-        else if (swatterIndex == 4)
+        else
         {
-
+            //insert a value here, i just don't know what to put yet
+            cityHealth -= 0;
+            arrayLength = 0;
+            while (arrayLength < eventZeroObjects.Length)
+            {
+                eventZeroObjects[arrayLength].SetActive(false);
+                arrayLength++;
+            }
+            eventActive = false;
         }
-        else if (swatterIndex == 5)
-        {
+    }
 
+    IEnumerator EventOne()
+    {
+        int arrayLength = 0;
+        while (arrayLength < eventOneObjects.Length)
+        {
+            eventOneObjects[arrayLength].SetActive(true);
+            arrayLength++;
         }
-        else if (swatterIndex == 6)
+        int timer = eventWaitTimer;
+        while (timer > 0 && !flyBoyMadeItToEvent)
         {
+            yield return new WaitForSecondsRealtime(1f);
+            timer--;
+        }
+        if (flyBoyMadeItToEvent)
+        {
+            FindAnyObjectByType<SkillCheck>().SkillCheckActivate();
+            while (FindAnyObjectByType<SkillCheck>().skillCheckActive)
+            {
+                yield return null;
+            }
+            if (FindAnyObjectByType<SkillCheck>().skillCheckHit)
+            {
+                FindAnyObjectByType<SkillCheck>().skillCheckHit = false;
+                CityHeal();
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
+            else
+            {
+                //insert a value here, i just don't know what to put yet
+                cityHealth -= 0;
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
+        }
+        else
+        {
+            //insert a value here, i just don't know what to put yet
+            cityHealth -= 0;
+            arrayLength = 0;
+            while (arrayLength < eventZeroObjects.Length)
+            {
+                eventZeroObjects[arrayLength].SetActive(false);
+                arrayLength++;
+            }
+            eventActive = false;
+        }
+    }
 
+    IEnumerator EventTwo()
+    {
+        int arrayLength = 0;
+        while (arrayLength < eventTwoObjects.Length)
+        {
+            eventTwoObjects[arrayLength].SetActive(true);
+            arrayLength++;
+        }
+        int timer = eventWaitTimer;
+        while (timer > 0 && !flyBoyMadeItToEvent)
+        {
+            yield return new WaitForSecondsRealtime(1f);
+            timer--;
+        }
+        if (flyBoyMadeItToEvent)
+        {
+            FindAnyObjectByType<SkillCheck>().SkillCheckActivate();
+            while (FindAnyObjectByType<SkillCheck>().skillCheckActive)
+            {
+                yield return null;
+            }
+            if (FindAnyObjectByType<SkillCheck>().skillCheckHit)
+            {
+                FindAnyObjectByType<SkillCheck>().skillCheckHit = false;
+                CityHeal();
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
+            else
+            {
+                //insert a value here, i just don't know what to put yet
+                cityHealth -= 0;
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
+        }
+        else
+        {
+            //insert a value here, i just don't know what to put yet
+            cityHealth -= 0;
+            arrayLength = 0;
+            while (arrayLength < eventZeroObjects.Length)
+            {
+                eventZeroObjects[arrayLength].SetActive(false);
+                arrayLength++;
+            }
+            eventActive = false;
+        }
+    }
+
+    IEnumerator EventThree()
+    {
+        int arrayLength = 0;
+        while (arrayLength < eventThreeObjects.Length)
+        {
+            eventThreeObjects[arrayLength].SetActive(true);
+            arrayLength++;
+        }
+        int timer = eventWaitTimer;
+        while (timer > 0 && !flyBoyMadeItToEvent)
+        {
+            yield return new WaitForSecondsRealtime(1f);
+            timer--;
+        }
+        if (flyBoyMadeItToEvent)
+        {
+            FindAnyObjectByType<SkillCheck>().SkillCheckActivate();
+            while (FindAnyObjectByType<SkillCheck>().skillCheckActive)
+            {
+                yield return null;
+            }
+            if (FindAnyObjectByType<SkillCheck>().skillCheckHit)
+            {
+                FindAnyObjectByType<SkillCheck>().skillCheckHit = false;
+                CityHeal();
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
+            else
+            {
+                //insert a value here, i just don't know what to put yet
+                cityHealth -= 0;
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
+        }
+        else
+        {
+            //insert a value here, i just don't know what to put yet
+            cityHealth -= 0;
+            arrayLength = 0;
+            while (arrayLength < eventZeroObjects.Length)
+            {
+                eventZeroObjects[arrayLength].SetActive(false);
+                arrayLength++;
+            }
+            eventActive = false;
+        }
+    }
+
+    IEnumerator EventFour()
+    {
+        int arrayLength = 0;
+        while (arrayLength < eventFourObjects.Length)
+        {
+            eventFourObjects[arrayLength].SetActive(true);
+            arrayLength++;
+        }
+        int timer = eventWaitTimer;
+        while (timer > 0 && !flyBoyMadeItToEvent)
+        {
+            yield return new WaitForSecondsRealtime(1f);
+            timer--;
+        }
+        if (flyBoyMadeItToEvent)
+        {
+            FindAnyObjectByType<SkillCheck>().SkillCheckActivate();
+            while (FindAnyObjectByType<SkillCheck>().skillCheckActive)
+            {
+                yield return null;
+            }
+            if (FindAnyObjectByType<SkillCheck>().skillCheckHit)
+            {
+                FindAnyObjectByType<SkillCheck>().skillCheckHit = false;
+                CityHeal();
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
+            else
+            {
+                //insert a value here, i just don't know what to put yet
+                cityHealth -= 0;
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
+        }
+        else
+        {
+            //insert a value here, i just don't know what to put yet
+            cityHealth -= 0;
+            arrayLength = 0;
+            while (arrayLength < eventZeroObjects.Length)
+            {
+                eventZeroObjects[arrayLength].SetActive(false);
+                arrayLength++;
+            }
+            eventActive = false;
+        }
+    }
+
+    IEnumerator EventFive()
+    {
+        int arrayLength = 0;
+        while (arrayLength < eventFiveObjects.Length)
+        {
+            eventFiveObjects[arrayLength].SetActive(true);
+            arrayLength++;
+        }
+        int timer = eventWaitTimer;
+        while (timer > 0 && !flyBoyMadeItToEvent)
+        {
+            yield return new WaitForSecondsRealtime(1f);
+            timer--;
+        }
+        if (flyBoyMadeItToEvent)
+        {
+            FindAnyObjectByType<SkillCheck>().SkillCheckActivate();
+            while (FindAnyObjectByType<SkillCheck>().skillCheckActive)
+            {
+                yield return null;
+            }
+            if (FindAnyObjectByType<SkillCheck>().skillCheckHit)
+            {
+                FindAnyObjectByType<SkillCheck>().skillCheckHit = false;
+                CityHeal();
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
+            else
+            {
+                //insert a value here, i just don't know what to put yet
+                cityHealth -= 0;
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
+        }
+        else
+        {
+            //insert a value here, i just don't know what to put yet
+            cityHealth -= 0;
+            arrayLength = 0;
+            while (arrayLength < eventZeroObjects.Length)
+            {
+                eventZeroObjects[arrayLength].SetActive(false);
+                arrayLength++;
+            }
+            eventActive = false;
+        }
+    }
+
+    IEnumerator EventSix()
+    {
+        int arrayLength = 0;
+        while (arrayLength < eventSixObjects.Length)
+        {
+            eventSixObjects[arrayLength].SetActive(true);
+            arrayLength++;
+        }
+        int timer = eventWaitTimer;
+        while (timer > 0 && !flyBoyMadeItToEvent)
+        {
+            yield return new WaitForSecondsRealtime(1f);
+            timer--;
+        }
+        if (flyBoyMadeItToEvent)
+        {
+            FindAnyObjectByType<SkillCheck>().SkillCheckActivate();
+            while (FindAnyObjectByType<SkillCheck>().skillCheckActive)
+            {
+                yield return null;
+            }
+            if (FindAnyObjectByType<SkillCheck>().skillCheckHit)
+            {
+                FindAnyObjectByType<SkillCheck>().skillCheckHit = false;
+                CityHeal();
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
+            else
+            {
+                //insert a value here, i just don't know what to put yet
+                cityHealth -= 0;
+                arrayLength = 0;
+                while (arrayLength < eventZeroObjects.Length)
+                {
+                    eventZeroObjects[arrayLength].SetActive(false);
+                    arrayLength++;
+                }
+                eventActive = false;
+            }
+        }
+        else
+        {
+            //insert a value here, i just don't know what to put yet
+            cityHealth -= 0;
+            arrayLength = 0;
+            while (arrayLength < eventZeroObjects.Length)
+            {
+                eventZeroObjects[arrayLength].SetActive(false);
+                arrayLength++;
+            }
+            eventActive = false;
         }
     }
 }
--- Assets/Scripts/SkillCheck.cs
+++ Assets/Scripts/SkillCheck.cs
@@ -1,24 +1,59 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SkillCheck : MonoBehaviour
 {
     [SerializeField] private GameObject hitIndicator;
     [SerializeField] private GameObject successZone;
+    public bool skillCheckActive;
+    private bool hit;
+    public bool skillCheckHit;
 
-    void SkillCheckActivate()
+    public void SkillCheckActivate()
     {
-        hitIndicator.transform.rotation = new Quaternion(hitIndicator.transform.rotation.x, hitIndicator.transform.rotation.y, 
-            Random.Range(0, 360),hitIndicator.transform.rotation.w);
+        hitIndicator.SetActive(true);
+        successZone.SetActive(true);
+        skillCheckActive = true;
+        successZone.transform.eulerAngles = new Vector4(successZone.transform.eulerAngles.x, successZone.transform.eulerAngles.y, 
+            Random.Range(0, 360), successZone.transform.rotation.w);
         StartCoroutine(SkillCheckSpin());
     }
 
     IEnumerator SkillCheckSpin()
     {
-        while (true)
+        float timer = 10;
+        while (timer > 0 && !hit)
         {
+            timer -= Time.deltaTime;
             yield return null;
-            //hitIndicator.transform.rotation == new Quaternion(hitIndicator.transform.rotation);
+            hitIndicator.transform.eulerAngles = new Vector4(hitIndicator.transform.eulerAngles.x, hitIndicator.transform.eulerAngles.y,
+                hitIndicator.transform.eulerAngles.z + 1, hitIndicator.transform.rotation.w);
+        }
+        if (hit)
+        {
+            hitIndicator.SetActive(false);
+            successZone.SetActive(false);
+            hit = false;
+            skillCheckHit = true;
+            skillCheckActive = false;
+        }
+        else
+        {
+            hitIndicator.SetActive(false);
+            successZone.SetActive(false);
+            skillCheckHit = false;
+            skillCheckActive = false;
+        }
+    }
+
+    void OnClick()
+    {
+        Debug.Log("click");
+        if (hitIndicator.transform.eulerAngles.z > successZone.transform.eulerAngles.z - 26 && 
+            hitIndicator.transform.eulerAngles.z < successZone.transform.eulerAngles.z + 26 && skillCheckActive)
+        {
+            hit = true;
         }
     }
 }
