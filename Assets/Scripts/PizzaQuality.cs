033425e1535f0e2659d643d57b3083a9e2574b7b
radical-ish <cruzmatt1015@gmail.com>
2026-04-15 11:49:07 -0500

    added money gain ui
    when getting a delivery point, amount of money earned appears

--- Assets/Scripts/PizzaQuality.cs
+++ Assets/Scripts/PizzaQuality.cs
@@ -15,11 +15,30 @@ public class PizzaQuality : MonoBehaviour
     [SerializeField] private GameObject penaltyLabel;
     private TMP_Text pentaltyText;
 
+    [SerializeField] private GameObject moneyGainLabel;
+    private TMP_Text moneyGainText;
+
     private void Start()
     {
         instance = this;
         pentaltyText = penaltyLabel.GetComponent<TMP_Text>();
+        moneyGainText = moneyGainLabel.GetComponent<TMP_Text>();
         rb = GetComponent<Rigidbody>();
+    }
+
+    public void DisplayMoney(float moneyGain)
+    {
+        StartCoroutine(DisplayMoneyCoroutine(moneyGain));
+    }
+
+    private IEnumerator DisplayMoneyCoroutine(float moneyGain)
+    {
+        string labelText = (Mathf.Round(moneyGain * 100)).ToString();
+        labelText = labelText.Substring(0, labelText.Length - 2) + "." + labelText.Substring(labelText.Length - 2);
+        moneyGainText.text = "+ $" + labelText;
+        moneyGainLabel.SetActive(true);
+        yield return new WaitForSeconds(labelDuration);
+        moneyGainLabel.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision collision)
