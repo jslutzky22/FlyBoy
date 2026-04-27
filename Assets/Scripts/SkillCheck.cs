19fe4c25d50ed4d5b04d5f957d8991059e647fe5
radical-ish <cruzmatt1015@gmail.com>
2026-04-27 13:04:54 -0500

    skill check uses unscaled time
    forgot to change one line

--- Assets/Scripts/SkillCheck.cs
+++ Assets/Scripts/SkillCheck.cs
@@ -32,7 +32,7 @@ public class SkillCheck : MonoBehaviour
         {
             yield return new WaitForSecondsRealtime(0.005f);
             hitIndicator.transform.eulerAngles = new Vector4(hitIndicator.transform.eulerAngles.x, hitIndicator.transform.eulerAngles.y,
-                hitIndicator.transform.eulerAngles.z + skillCheckSpinSpeed * Time.deltaTime, hitIndicator.transform.rotation.w);
+                hitIndicator.transform.eulerAngles.z + skillCheckSpinSpeed * Time.unscaledDeltaTime, hitIndicator.transform.rotation.w);
             if (miss)
             {
                 hitIndicator.GetComponentInChildren<Image>().color = Color.red;
